155d8e824be686b01dbec6b3e4b0f980d59b7030
Suman Katwal <katwalsuman12@gmail.com>
2024-01-04 17:27:31 +1100

    till template string

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const saveButton = document.getElementById("input-btn");
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
+let ulEl = document.getElementById('ul-el')
 
-saveButton.addEventListener("click", function(){
-    console.log("button clicked!");
+saveButton.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 });
 
+
+function renderLeads() {
+    let listItems = "";
+
+    for (let i = 0; i < myLeads.length; i++) {
+       // listItems += "<li><a href ='" + myLeads[i] + "' target='_blank' "+ ">"+  myLeads[i] + "</li>"
+listItems+= `
+        <li>
+            <a href = '${myLeads[i]}' target='_blank'>${myLeads[i]}
+            </a>
+        </li>
+`
+    }
+
+    ulEl.innerHTML = listItems
+}
