aa72cdec6bcfdfa97466a7214ea3b9db3e2ca5d4
Suman Katwal <katwalsuman12@gmail.com>
2024-01-05 00:53:27 +1100

    uploaded to Chrome

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ let myLeads = [];
 const inputEl = document.getElementById('input-el');
 let ulEl = document.getElementById('ul-el')
 
+
+console.log(localStorage.getItem('myLeads'))
+
+
+
+
 saveButton.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
@@ -14,10 +20,11 @@ function renderLeads() {
     let listItems = "";
 
     for (let i = 0; i < myLeads.length; i++) {
-       // listItems += "<li><a href ='" + myLeads[i] + "' target='_blank' "+ ">"+  myLeads[i] + "</li>"
-listItems+= `
+        // listItems += "<li><a href ='" + myLeads[i] + "' target='_blank' "+ ">"+  myLeads[i] + "</li>"
+        listItems += `
         <li>
-            <a href = '${myLeads[i]}' target='_blank'>${myLeads[i]}
+            <a href = '${myLeads[i]}' target='_blank'>
+            ${myLeads[i]}
             </a>
         </li>
 `
