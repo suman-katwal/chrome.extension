361f1527aa8806af1edd016d9b8fd32584cdd44b
Suman Katwal <katwalsuman12@gmail.com>
2024-01-04 15:55:40 +1100

    04/01 commit

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-let saveButton = document.getElementById("input-btn");
+const saveButton = document.getElementById("input-btn");
+let myLeads = [];
+const inputEl = document.getElementById('input-el');
 
 saveButton.addEventListener("click", function(){
     console.log("button clicked!");
 });
 
-// Create two variables:
-// myLeads -> should be assigned to an empty array
-// inputEl -> should be assigned to the text input field
-
-let myLeads = [];
-let inputEl = document.getElementById('input-el');
